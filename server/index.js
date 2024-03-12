6cbe08598997768d8c62bc7eca33944f8161b001
irene <panisirene@gmail.com>
2024-03-12 14:51:52 -0700

    mount routes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const routes = require('./routes');
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', routes); // Mount all routes under the /api base URL
 
 app.listen(5000, () => {
   console.log(`Listening on port 5000!`)

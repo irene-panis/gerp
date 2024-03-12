e2955cab2633d67f95e33744884cf408ca811ff6
irene <panisirene@gmail.com>
2024-03-12 15:01:05 -0700

    fix profile routes

--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const profileController = require('../controllers/profileController');
 
 // Route to create a new profile
-router.post('/profiles', profileController.createProfile);
+router.post('/', profileController.createProfile);
 
 // Route to get all profiles
-router.get('/profiles', profileController.getAllProfiles);
+router.get('/', profileController.getAllProfiles);
 
 module.exports = router;
